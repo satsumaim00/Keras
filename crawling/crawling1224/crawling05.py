cfebf752ee35c441179a38b7c6c7557dab4f7adf
satsumaim00 <48466667+satsumaim00@users.noreply.github.com>
2019-12-24 17:31:15 +0900

    Add files via upload

---
+++ crawling/crawling1224/crawling05.py
@@ -0,0 +1,9 @@
+import csv, codecs
+
+with codecs.open("test.csv", "w", "euc_kr") as fp:  #실행시 text이름으로 엑셀저장됨
+    writer = csv.writer(fp, delimiter=",", quotechar='"')
+    writer.writerow(["ID", "이름", "가격"])
+    writer.writerow(["1000", "SD 카드 ", 30000])
+    writer.writerow(["1001", "키보드", 21000])
+    writer.writerow(["1002", "마우스 ", 15000])
+
