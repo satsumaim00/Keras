ffc92de655b1b5a03c44cc4db7796006f10bceb1
satsumaim00 <48466667+satsumaim00@users.noreply.github.com>
2019-12-24 17:39:18 +0900

    Delete crawling06.py

--- crawling06.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-headers = {'User-Agent': 'Mozilla/5.0'}
-url = "http://jolse.com/category/toners-mists/1019/"
-
-result = requests.get(url , headers=headers)
-
-bs_obj = BeautifulSoup(result.text)
-print(bs_obj)
